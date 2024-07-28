5871e6a28f9d8ba56a4bd1b3b7d7d79590b42ca2
krzysztof <larkfreeme70.55@gmail.com>
2024-07-28 17:19:29 +0200

    feat(app-ui): add sample item data mock

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import exercises from "./assets/data/exercises.json"; // TODO: implement api
+
+// TODO: to utils
+const capitalizeFirstLetter = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 export default function App() {
   const exercise = exercises[0];
 
   return (
     <View style={styles.container}>
+      <View style={styles.exerciseContainer}>
+        <Text style={styles.exerciseName}>{exercise.name}</Text>
+        <Text style={styles.exerciseSubtitle}>
+          {capitalizeFirstLetter(exercise.muscle)} |{" "}
+          {capitalizeFirstLetter(exercise.equipment)}
+        </Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +30,18 @@ const styles = StyleSheet.create({
     backgroundColor: "gainsboro",
     justifyContent: "center",
     padding: 10,
+  },
+  exerciseContainer: {
+    gap: 5,
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: "#fff",
+  },
+  exerciseName: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  exerciseSubtitle: {
+    color: "dimgray",
   },
 });
